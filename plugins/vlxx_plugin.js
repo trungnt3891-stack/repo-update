7bd4c84c933d2aeae5b5b05aa8de052102832bf5
youngbi <32502366+youngbi@users.noreply.github.com>
2026-04-22 15:40:02 +0700

    Update vlxx_plugin.js

--- plugins/vlxx_plugin.js
+++ plugins/vlxx_plugin.js
@@ -6,7 +6,7 @@ function getManifest() {
     return JSON.stringify({
         "id": "vlxx",
         "name": "VLXX",
-        "version": "1.0.1",
+        "version": "1.0.2",
         "baseUrl": "https://vlxx.moi",
         "iconUrl": "https://raw.githubusercontent.com/youngbi/repo/main/plugins/vlxx.ico",
         "isEnabled": true,
